8d1622f7178fffc74e70a1ecb0192ad385702c99
Christian Feldmann <christian.feldmann@gmx.de>
2019-08-08 17:23:19 +0200

    Added missing files of the ws281x library

--- output/output.cpp
+++ output/output.cpp
@@ -9,38 +9,27 @@
 //#define STRIP_TYPE            WS2811_STRIP_RGB		// WS2812/SK6812RGB integrated chip+leds
 #define STRIP_TYPE              WS2811_STRIP_GBR		// WS2812/SK6812RGB integrated chip+leds
 //#define STRIP_TYPE            SK6812_STRIP_RGBW		// SK6812RGBW (NOT SK6812RGB)
+#define LED_COUNT               489
 
 Output::Output()
 {
-    ws2811_t ledstring =
-    {
-        .freq = TARGET_FREQ,
-        .dmanum = DMA,
-        .channel =
-        {
-            [0] =
-            {
-                .gpionum = GPIO_PIN,
-                .count = LED_COUNT,
-                .invert = 0,
-                .brightness = 255,
-                .strip_type = STRIP_TYPE,
-            },
-            [1] =
-            {
-                .gpionum = 0,
-                .count = 0,
-                .invert = 0,
-                .brightness = 0,
-            },
-        },
-    };
+    ws2811_t ledstring;
+    ledstring.freq = TARGET_FREQ;
+    ledstring.dmanum = DMA;
+    ledstring.channel[0].gpionum = GPIO_PIN;
+    ledstring.channel[0].count = LED_COUNT;
+    ledstring.channel[0].invert = 0;
+    ledstring.channel[0].brightness = 255;
+    ledstring.channel[0].strip_type = STRIP_TYPE;
+    ledstring.channel[1].gpionum = 0;
+    ledstring.channel[1].count = 0;
+    ledstring.channel[1].invert = 0;
+    ledstring.channel[1].brightness = 0;
 
+    ws2811_return_t ret;
     if ((ret = ws2811_init(&ledstring)) != WS2811_SUCCESS)
     {
         fprintf(stderr, "ws2811_init failed: %s\n", ws2811_get_return_t_str(ret));
-        return ret;
     }
-
 }
 
